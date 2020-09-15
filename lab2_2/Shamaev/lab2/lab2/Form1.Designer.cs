70bf2c89f1f6ea4555cb111710fa321a74e4f10d
Dr0bash <37270533+Dr0bash@users.noreply.github.com>
2020-09-15 15:54:56 +0300

    Add files via upload

---
+++ lab2_2/Shamaev/lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(631, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Преобразовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(582, 93);
+            this.trackBar1.Maximum = 360;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(230, 45);
+            this.trackBar1.TabIndex = 2;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Enabled = false;
+            this.trackBar2.Location = new System.Drawing.Point(582, 172);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Minimum = -100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(230, 45);
+            this.trackBar2.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(664, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(66, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Hue";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(664, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(66, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Saturation";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(664, 224);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(66, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Value";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Enabled = false;
+            this.trackBar3.Location = new System.Drawing.Point(582, 249);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Minimum = -100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(230, 45);
+            this.trackBar3.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(560, 467);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 48);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Загрузить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(631, 382);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 45);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 491);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab2_2/Shamaev/lab2/lab2/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bitmaporigin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            OpenFileDialog myfile = new OpenFileDialog();
+            myfile.Title = "First step Site";
+            myfile.Filter = "png files (*.png)|*.png|All files (*.*)|*.*";
+            if (myfile.ShowDialog() == DialogResult.OK)
+            {
+                path = myfile.FileName;
+                pictureBox1.Image = Image.FromFile(path);
+            }
+            bitmaporigin = new Bitmap(pictureBox1.Image);
+            button1.Enabled = true;
+            button3.Enabled = true;
+            trackBar1.Enabled = true;
+            trackBar2.Enabled = true;
+            trackBar3.Enabled = true;
+        }
+
+        private int Max3(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                    return a;
+                else
+                    return c;
+            }
+            else
+            {
+                if (b > c)
+                    return b;
+                else
+                    return c;
+            }
+        }
+        private int Min3(int a, int b, int c)
+        {
+            if (a < b)
+            {
+                if (a < c)
+                    return a;
+                else
+                    return c;
+            }
+            else
+            {
+                if (b < c)
+                    return b;
+                else
+                    return c;
+            }
+        }
+
+        private double GetHue(ref int r, ref int g, ref int b, ref int ormax, ref int ormin)
+        {
+            double red = r / 255.0;
+            double green = g / 255.0;
+            double blue = b / 255.0;
+            double max = ormax / 255.0;
+            double min = ormin / 255.0;
+            if (ormax == ormin)
+            {
+                return 0;
+            }
+            else if (ormax == r)
+            {
+                if (green >= blue)
+                    return (60 * (green - blue)/(max - min));
+                else
+                    return (60 * (green - blue) / (max - min) + 360);
+            }
+            else if (ormax == g)
+            {
+                return (60 * (blue - red) / (max - min) + 120);
+            }
+            else
+            {
+                return (60 * (red - green) / (max - min) + 240);
+            }
+        }
+
+        private double GetSaturation(ref int ormax, ref int ormin)
+        {
+            double max = ormax / 255.0;
+            double min = ormin / 255.0;
+            if (ormax == 0)
+                return 0;
+            else
+                return ((1 - min / max));
+        }
+
+        private Tuple<double, double, double> RGB_To_HSV(Color color)
+        {
+            int red = color.R;
+            int green = color.G;
+            int blue = color.B;
+            int max = Max3(red, green, blue);
+            int min = Min3(red, green, blue);
+
+            return new Tuple<double, double, double>(GetHue(ref red, ref green, ref blue, ref max, ref min), 
+                                                     GetSaturation(ref max, ref min), 
+                                                     max / 255.0);
+        }
+
+        private Color HSV_To_RGB(Tuple<double, double, double> hsv)
+        {
+            int H = (int)Math.Floor(hsv.Item1 / 60.0) % 6;
+            double f = (hsv.Item1 / 60.0) - (int)Math.Floor(hsv.Item1 / 60.0);
+            double p = hsv.Item3 * (1 - hsv.Item2);
+            double q = hsv.Item3 * (1 - f*hsv.Item2);
+            double t = hsv.Item3 * (1 - (1 - f)*hsv.Item2);
+            switch (H)
+            {
+                case 0:
+                    return Color.FromArgb((int)(hsv.Item3 * 255), (int)(t * 255), (int)(p * 255));
+                case 1:
+                    return Color.FromArgb((int)(q * 255), (int)(hsv.Item3 * 255), (int)(p * 255));
+                case 2:
+                    return Color.FromArgb((int)(p * 255), (int)(hsv.Item3 * 255), (int)(t * 255));
+                case 3:
+                    return Color.FromArgb((int)(p * 255), (int)(q * 255), (int)(hsv.Item3 * 255));
+                case 4:
+                    return Color.FromArgb((int)(t * 255), (int)(p * 255), (int)(hsv.Item3 * 255));
+                case 5:
+                    return Color.FromArgb((int)(hsv.Item3 * 255), (int)(p * 255), (int)(q * 255));
+                default:
+                    return Color.Black;
+            }
+        }
+
+        private double CheckBordersSatValue(double x)
+        {
+            if (x > 1)
+                return 1;
+            else if (x < 0)
+                return 0;
+            else
+                return x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(bitmaporigin);
+            for (int x = 0; x < bitmap.Width; ++x)
+            {
+                //int a = 0;
+                for (int y = 0; y < bitmap.Height; ++y)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    var pix = RGB_To_HSV(pixelColor);
+                    var newpix = new Tuple<double, double, double>(pix.Item1 + trackBar1.Value, 
+                                                                   CheckBordersSatValue(pix.Item2 + (trackBar2.Value / 100.0)), 
+                                                                   CheckBordersSatValue(pix.Item3 + (trackBar3.Value / 100.0)));
+                    Color newcolor = HSV_To_RGB(newpix);
+                    bitmap.SetPixel(x, y, newcolor);
+                }
+            }
+            pictureBox1.Image = bitmap;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|PNG Image|*.png";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs =
+                    (System.IO.FileStream)saveFileDialog1.OpenFile();
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        pictureBox1.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        pictureBox1.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                }
+                fs.Close();
+            }
+        }
+    }
+}
