73010be9c0bea2906f94b690159e2d5b02c2653d
Dr0bash <37270533+Dr0bash@users.noreply.github.com>
2020-09-20 14:53:05 +0300

    task3 in lab_3 is done

---
+++ lab3/task3/task3/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(778, 586);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(797, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(810, 350);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "Алгоритм Ву";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(851, 240);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Visible = false;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(837, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Толщина пера";
+            this.label1.Visible = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(810, 323);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(146, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Алгоритм Брезенхейма";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 611);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ lab3/task3/task3/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        bool begin;
+        Point point1, point2;
+        int penwidth = 1;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            begin = false;
+            pictureBox1.BackColor = Color.White;
+        }
+
+        private Tuple<int, int> Direction(Point a, Point b)
+        {
+            int x = 0;
+            int y = 0;
+
+            if (a.X > b.X)
+                x = -1;
+            else if (a.X < b.X)
+                x = 1;
+
+            if (a.Y > b.Y)
+                y = -1;
+            else if (a.Y < b.Y)
+                y = 1;
+
+            return new Tuple<int, int>(x, y);
+        }
+
+        private void Bretzenheim(Graphics graphics, Pen p, Point a, Point b)
+        {
+            Bitmap bm = new Bitmap(1, 1);
+
+            double dx = Math.Abs(b.X - a.X);
+            double dy = Math.Abs(b.Y - a.Y);
+            double grad = dy/dx;
+            var txy = Direction(a, b);
+            int dirx = txy.Item1;
+            int diry = txy.Item2;
+            int x = a.X;
+            int y = a.Y;
+            if (grad <= 1)
+            {
+                double d = 2 * dy - dx;
+                while (x != b.X || y != b.Y)
+                {
+                    
+                    if (d < 0)
+                    {
+                        d = d + 2 * dy;
+                    }
+                    else
+                    {
+                        y += diry;
+                        d = d + 2 * (dy - dx);
+                    }
+                    x += dirx;
+                    bm.SetPixel(0, 0, Color.Red);
+                    graphics.DrawImageUnscaled(bm, x, y);
+                }
+            }
+            else 
+            {
+                double d = 2 * dx - dy;
+                while (x != b.X || y != b.Y)
+                {
+
+                    if (d < 0)
+                    {
+                        d = d + 2 * dx;
+                    }
+                    else
+                    {
+                        x += dirx;
+                        d = d + 2 * (dx - dy);
+                    }
+                    y += diry;
+                    bm.SetPixel(0, 0, Color.Red);
+                    graphics.DrawImageUnscaled(bm, x, y);
+                }
+            }
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        private void DrawPoint(Graphics graphics , bool steep, int x, int y, double grade)
+        {
+            grade = 1 - grade;
+            Bitmap bm = new Bitmap(1, 1);
+            bm.SetPixel(0, 0, Color.FromArgb((int)(255 * grade), (int)(255 * grade), (int)(255 * grade)));
+            if (steep)
+                graphics.DrawImageUnscaled(bm, y, x);
+            else
+                graphics.DrawImageUnscaled(bm, x, y);
+        }
+
+        private void Wu(Graphics graphics, Pen p, int x0, int y0, int x1, int y1)
+        {
+            bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
+            if (steep)
+            {
+                Swap(ref x0, ref y0);
+                Swap(ref x1, ref y1);
+            }
+            if (x0 > x1)
+            {
+                Swap(ref x0, ref x1);
+                Swap(ref y0, ref y1);
+            }
+
+            DrawPoint(graphics, steep, x0, y0, 1);
+            DrawPoint(graphics, steep, x1, y1, 1);
+            float dx = x1 - x0;
+            float dy = y1 - y0;
+            float gradient = dy / dx;
+            float y = y0 + gradient;
+            for (var x = x0 + 1; x <= x1 - 1; x++)
+            {
+                DrawPoint(graphics, steep, x, (int)y, 1 - (y - (int)y));
+                DrawPoint(graphics, steep, x, (int)y + 1, y - (int)y);
+                y += gradient;
+            }
+        }
+
+        private void MyPaint(Graphics graphics, Point a, Point b)
+        {
+            Pen p = new Pen(Color.Black, penwidth);
+            if (checkBox2.Checked)
+                Bretzenheim(graphics, p, a, b);
+            else
+                Wu(graphics, p, a.X,a.Y,b.X,b.Y);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            penwidth = (int)numericUpDown1.Value;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+                checkBox1.Checked = false;
+            else
+                checkBox1.Checked = true;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                checkBox2.Checked = false;
+            else
+                checkBox2.Checked = true;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (begin)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                point2 = new Point(e.X, e.Y);
+                begin = false;
+                MyPaint(g, point1, point2);
+            }
+            else
+            {
+                begin = true;
+                point1 = new Point(e.X, e.Y);
+            }
+        }
+    }
+}
