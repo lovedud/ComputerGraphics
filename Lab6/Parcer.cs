29274de6d988819134316efcc9d63be0b51dfa21
Dr0bash <igor2-shamaev@yandex.ru>
2020-10-30 15:47:15 +0300

    Revert "try to return  master"
    This reverts commit 8b88516c38a200762bb4cb1e5840658aff78692f.

---
+++ Lab6/Parcer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static Affin3D.AffinStuff;
+
+namespace Affin3D
+{
+    class Parcer
+    {
+        public List<Polyhedron> ParceFromFile(string fname)
+        {
+            List<Polyhedron> res = new List<Polyhedron>();
+            bool constructing_polyhedr = false;
+            List<Point3D> cur_points = new List<Point3D>();
+            List<List<int>> cur_polygons = new List<List<int>>();
+            using (StreamReader sr = new StreamReader(fname))
+            {
+                string line = sr.ReadLine();
+                while(line != null)
+                {  
+                    char id = line == "" ? '#' : line[0];
+                    
+                    if (id == 'v')
+                    {
+                        char NextChar = line[1];
+                        if (NextChar != 't' && NextChar != 'n' && NextChar != 'p')
+                        {
+                            if (constructing_polyhedr)
+                            {
+                                constructing_polyhedr = false;
+                                res.Add(new Polyhedron(cur_points, cur_polygons));
+                                cur_points = new List<Point3D>();
+                                cur_polygons = new List<List<int>>();
+                            }
+                            cur_points.Add(ParcePoint(line));
+                        }
+                        
+                    }
+                    else if (id == 'f')
+                    {
+                        constructing_polyhedr = true;
+                        cur_polygons.Add(ParcePolygon(line));
+                    }
+                    line = sr.ReadLine();
+                }
+            }
+            constructing_polyhedr = false;
+            res.Add(new Polyhedron(cur_points, cur_polygons));
+            return res;
+        }
+        private Point3D ParcePoint(string line)
+        {
+            Point3D res = new Point3D();
+            var tokens = line.Substring(1).Trim().Split(' ').SkipWhile((x) => x == "").ToArray(); 
+            if (tokens.Length < 2)
+                throw new InvalidCastException("Point parse fail");
+            res.X = float.Parse(tokens[0].Replace('.',','));
+            res.Y = float.Parse(tokens[1].Replace('.', ','));
+            res.Z = float.Parse(tokens[2].Replace('.', ','));
+            return res;
+        }
+        private List<int> ParcePolygon(string line)
+        {
+            List<int> res = new List<int>();
+            var tokens = line.Substring(1).Trim().Split(' ').SkipWhile((x) => x == "").ToArray();
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                var vertex_ind = int.Parse(tokens[i].Split('/')[0]);
+                res.Add(vertex_ind - 1);
+            }
+            return res;
+        }
+       
+    }
+}
